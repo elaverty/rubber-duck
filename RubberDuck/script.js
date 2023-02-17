b96dd4eddf903c39c781bedecbc4e6e6194c3ac3
Liz Laverty (she/her) <elaverty@users.noreply.github.com>
2023-02-17 16:02:18 -0500

    Add files via upload

---
+++ RubberDuck/script.js
@@ -0,0 +1,7 @@
+function setup() {
+	chatbot.loadFiles(['brain/hello.rive', 'brain/begin.rive','brain/star.rive', 'brain/admin.rive', 'brain/clients.rive', 'brain/coffee.rive', 'brain/eliza.rive', 'brain/hello.rive', 'brain/javascript.rive', 'brain/myself.rive', 'brain/rpg.rive', 'brain/star.rive',
+	'brain/errors.rive',
+	'brain/students.rive' ]);
+}
+
+window.onload = setup;
